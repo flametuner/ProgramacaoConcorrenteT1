0ee32b6e9edeab22c90eaa33e70d01898fe1c00b
Gustavo Inacio <gugu.raimundo@gmail.com>
2018-05-11 10:56:33 -0300

    Implementada fila

--- fila.c
+++ fila.c
@@ -7,25 +7,48 @@
  **/
 
  elemento_t * aloca_elemento (aviao_t * dado) {
-    return NULL;
+    elemento_t* elemento = malloc(sizeof(dado));
+    elemento->dado = dado;
+    return elemento;
  }
 
  void desaloca_elemento (elemento_t * elemento) {
-
+   free(elemento);
  }
 
 fila_ordenada_t * criar_fila () {
-    return NULL;
+    fila_ordenada_t* fila = malloc(sizeof(fila_ordenada_t));
+    fila->n_elementos = 0;
+    return fila;
 }
 
 void desaloca_fila (fila_ordenada_t * fila) {
-
+  free(fila);
 }
 
 void inserir (fila_ordenada_t * fila, aviao_t * dado) {
-
+  elemento_t* elemento = aloca_elemento(dado);
+  if(fila->n_elementos == 0) {
+    fila->primeiro = elemento;
+    fila->ultimo = elemento;
+  } else {
+    fila->ultimo->proximo = elemento;
+    elemento->anterior = fila->ultimo;
+    fila->ultimo = elemento;
+  }
 }
 
 aviao_t * remover (fila_ordenada_t * fila) {
-  return NULL;
+  aviao_t* aviao;
+  if(fila->n_elementos > 1) {
+    aviao = fila->primeiro->dado;
+    elemento_t t = fila->primeiro;
+    fila->primeiro = t->proximo;
+    desaloca_elemento(t);
+  } else {
+    aviao = fila->primeiro->dado;
+    fila->primeiro = NULL;
+    fila->ultimo = NULL;
+  }
+  return aviao;
 }
